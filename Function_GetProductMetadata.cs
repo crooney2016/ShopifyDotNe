a92ef7748dbffed14c47f70eee9352ab58404350
crooney2016 <32747635+crooney2016@users.noreply.github.com>
2023-01-18 04:13:10 -0600

    Update

--- Function_GetProductMetadata.cs
+++ Function_GetProductMetadata.cs
@@ -12,22 +12,18 @@ namespace Shopify.Functions
 {
     public static class GetProductMetadata
     {
-        namespace Shopify.Functions
-    {
-        public static class GetProductMetadata
+        [FunctionName("GetProductMetadata")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
         {
-            [FunctionName("GetProductMetadata")]
-            public static async Task<IActionResult> Run(
-        [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
-        ILogger log)
-            {
-                log.LogInformation("C# HTTP trigger function processed a request.");
+            log.LogInformation("C# HTTP trigger function processed a request.");
 
-                // Get store id from request 
-                string storeID = req.Query["storeID"];
+            // Get store id from request 
+            string storeID = req.Query["storeID"];
 
-                // Create request for products in store 
-                string query = @"query {
+            // Create request for products in store 
+            string query = @"query {
       shop {
         products(first: 10) {
           pageInfo {
@@ -50,31 +46,31 @@ namespace Shopify.Functions
         }
       }
     }";
-                var request = WebRequest.CreateHttp("https://" + storeID + ".myshopify.com/admin/api/2020-01/graphql.json");
-                var postData = Encoding.UTF8.GetBytes(query);
+            var request = WebRequest.CreateHttp("https://" + storeID + ".myshopify.com/admin/api/2020-01/graphql.json");
+            var postData = Encoding.UTF8.GetBytes(query);
 
-                request.Method = "POST";
-                request.ContentType = "application/json";
-                request.ContentLength = postData.Length;
+            request.Method = "POST";
+            request.ContentType = "application/json";
+            request.ContentLength = postData.Length;
 
-                using (var stream = request.GetRequestStream())
-                {
-                    stream.Write(postData, 0, postData.Length);
-                }
+            using (var stream = request.GetRequestStream())
+            {
+                stream.Write(postData, 0, postData.Length);
+            }
 
-                // Declare variables 
-                var response = (HttpWebResponse)request.GetResponse();
-                var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
-                dynamic products = JsonConvert.DeserializeObject(responseString);
-                var productMetadata = new List<dynamic>();
+            // Declare variables 
+            var response = (HttpWebResponse)request.GetResponse();
+            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+            dynamic products = JsonConvert.DeserializeObject(responseString);
+            var productMetadata = new List<dynamic>();
 
-                // Loop through all products in store 
-                while (products.data.shop.products.pageInfo.hasNextPage)
-                {
-                    productMetadata.AddRange(products.data.shop.products.edges.Select(edge => edge.node));
+            // Loop through all products in store 
+            while (products.data.shop.products.pageInfo.hasNextPage)
+            {
+                productMetadata.AddRange(products.data.shop.products.edges.Select(edge => edge.node));
 
-                    // Create request for next page of products 
-                    query = @"query {
+                // Create request for next page of products 
+                query = @"query {
           shop {
             products(first: 10, after: """ + products.data.shop.products.pageInfo.endCursor + @""") {
               pageInfo {
@@ -97,23 +93,22 @@ namespace Shopify.Functions
             }
           }
         }";
-                    postData = Encoding.UTF8.GetBytes(query);
+                postData = Encoding.UTF8.GetBytes(query);
 
-                    request.ContentLength =
-            request.ContentLength = postData.Length;
+                request.ContentLength =
+                    request.ContentLength = postData.Length;
 
-                    using (var stream = request.GetRequestStream())
-                    {
-                        stream.Write(postData, 0, postData.Length);
-                    }
-
-                    response = (HttpWebResponse)request.GetResponse();
-                    responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
-                    products = JsonConvert.DeserializeObject(responseString);
-
-                    // Return product metadata in response 
-                    return new OkObjectResult(productMetadata);
+                using (var stream = request.GetRequestStream())
+                {
+                    stream.Write(postData, 0, postData.Length);
                 }
+
+                response = (HttpWebResponse)request.GetResponse();
+                responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+                products = JsonConvert.DeserializeObject(responseString);
+
+                // Return product metadata in response 
+                return new OkObjectResult(productMetadata);
             }
         }
     }
